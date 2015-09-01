c17dac2651d0dd64b3040c3462af2661fd95ecec
Trevor Steele <tsteele@elevenfiftyconsulting.com>
2015-09-01 15:37:50 -0400

    Installed Game, about to build 11note

---
+++ ElevenNote.Web/Controllers/GameController.cs
@@ -0,0 +1,44 @@
+﻿using ElevenNote.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.Web.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        [HttpGet]
+        [ActionName("Index")]
+        public ActionResult IndexGet()
+        {
+            var correctAnswer = new Random().Next(1, 10);
+            Session["Answer"] = correctAnswer;
+            return View();
+        }
+
+
+        // Post: Game
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult IndexPost(GuessingGameViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Guess == (int)Session["Answer"])
+                {
+                    ViewBag.Win = true;
+                }
+                else
+                {
+                    ViewBag.Win = false;
+                }
+            }
+
+            return View(model);
+        }
+    }
+
+}
---
+++ ElevenNote.Web/Models/GuessingGameViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ElevenNote.Web.Models
+{
+    public class GuessingGameViewModel
+    {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(20)]
+        [Display(Name="Your Name")]
+        public String Name { get; set; }
+
+        [Required]
+        [Range(1, 10)]
+        [Display (Name="Your Guess")]
+        public int Guess { get; set; }
+
+    }
+}
