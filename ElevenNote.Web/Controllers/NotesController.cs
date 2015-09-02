5178439127c5f961ba7e62b76418258fe7718f97
Trevor Steele <tsteele@elevenfiftyconsulting.com>
2015-09-02 14:04:58 -0400

    2 pm break. basic notes list working w/o database

---
+++ ElevenNote.Web/Controllers/NotesController.cs
@@ -0,0 +1,33 @@
+﻿using ElevenNote.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.Web.Controllers
+{
+    public class NotesController : Controller
+    {
+        // GET: Notes
+        public ActionResult Index()
+        {
+            var notes = new List<NoteListViewModel>();
+
+            notes.Add(new NoteListViewModel()
+            {
+                Id = 0,
+                DateCreated = DateTime.UtcNow.AddMonths(-1),
+                DateModified = DateTime.UtcNow,
+                IsFavorite = true,
+                Title = "Some note title"
+
+            });
+            
+
+            
+
+            return View(notes);
+        }
+    }
+}
