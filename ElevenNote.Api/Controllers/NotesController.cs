d01ca4364658f214d9552e5a98a216baa9096b49
Trevor Steele <tsteele@elevenfiftyconsulting.com>
2015-09-04 14:00:14 -0400

    End of Day 7

---
+++ ElevenNote.Api/Controllers/NotesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace ElevenNote.Api.Controllers
+{
+    [Authorize]
+    public class NotesController : ApiController
+    {
+        ///<summary
+        /// Gets all the notes for the current user.
+        /// </summary>
+        /// <returns></returns>
+        
+       
+        public IHttpActionResult GetNotes()
+        {
+            var service = new Services.NoteService();
+            var userId = User.Identity.GetUserId();
+            var notes = service.GetAllForUser(Guid.Parse(userId));
+
+            return Ok(notes);
+        }
+    }
+}
